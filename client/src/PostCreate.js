6ddb7cdf59f8e4fc0d1da34ebc4c0b210a4b2576
Rafa Moreno <rmh1516@gmail.com>
2020-06-11 10:54:07 +0200

    add post create component

---
+++ client/src/PostCreate.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const PostCreate = () => {
+  const [title, setTitle] = useState('')
+  const handleChange = event => {
+    setTitle(event.target.value)
+  }
+  const handleSubmit = async event => {
+    event.preventDefault()
+    await axios.post(`${process.env.REACT_APP_POSTS_SERVICE}/posts`, {
+      title,
+    })
+    setTitle('')
+  }
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Create Post</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="field">
+            <label className="label">Title</label>
+            <div className="control">
+              <input value={title} onChange={handleChange} className="input" />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button className="button is-link">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default PostCreate
