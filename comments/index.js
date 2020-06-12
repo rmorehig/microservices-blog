2d05d175e3001d02a14d2ac859eedbcb9332e6f3
Rafa Moreno <rmh1516@gmail.com>
2020-06-12 10:40:43 +0200

    add new moderation events

--- comments/index.js
+++ comments/index.js
@@ -26,18 +26,30 @@ app.post('/posts/:id/comments', async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: 'pending',
     },
-  })
-  console.log({
-    id: commentId,
-    content,
-    postId: req.params.id,
   })
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received event', req.body.type)
+  const { type, data } = req.body
+  if (type === 'CommentModerated') {
+    const { id, postId, status } = data
+    const comments = commentsByPostId[postId]
+    const comment = comments.find(comment => comment.id === id)
+    comment.status = status
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    })
+  }
   res.send({})
 })
 
--- moderation/index.js
+++ moderation/index.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received event', req.body.type)
   const { type, data } = req.body
-
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data
+    const status = content.includes('orange') ? 'rejected' : 'approved'
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    })
+  }
   res.send({})
 })
 
