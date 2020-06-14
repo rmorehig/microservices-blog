60a971fa2097823de095d107add383ff5bc15036
Rafa Moreno <rmh1516@gmail.com>
2020-06-14 17:06:49 +0200

    add rest of routes to ingress config

--- client/src/api.js
+++ client/src/api.js
@@ -1,26 +1,26 @@
 import axios from 'axios'
 
 export const addPost = async ({ title }) => {
-  await axios.post(`${process.env.REACT_APP_POSTS_SERVICE}/posts`, {
-    title,
+  await axios.post(`${process.env.REACT_APP_BACKEND_URL}/posts/create`, {
+    title
   })
 }
 
 export const getPosts = async () => {
-  return await axios.get(`${process.env.REACT_APP_QUERY_SERVICE}/posts`)
+  return await axios.get(`${process.env.REACT_APP_BACKEND_URL}/posts`)
 }
 
 export const addComment = async ({ postId, content }) => {
   await axios.post(
-    `${process.env.REACT_APP_COMMENTS_SERVICE}/posts/${postId}/comments`,
+    `${process.env.REACT_APP_BACKEND_URL}/posts/${postId}/comments`,
     {
-      content,
+      content
     }
   )
 }
 
 export const getComments = async ({ postId }) => {
   return await axios.get(
-    `${process.env.REACT_APP_COMMENTS_SERVICE}/posts/${postId}/comments`
+    `${process.env.REACT_APP_BACKEND_URL}/posts/${postId}/comments`
   )
 }
