3837fd99b7a2b6c595245c87e588d48962e69120
Rafa Moreno <rmh1516@gmail.com>
2020-06-14 15:46:59 +0200

    update post and event bus links for kubernetes communication

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json())
 app.post('/events', (req, res) => {
   const event = req.body
   events.push(event)
-  axios.post('http://localhost:4000/events', event)
-  axios.post('http://localhost:4001/events', event)
+  axios.post('http://posts-clusterip-srv:4000/events', event)
+  /*   axios.post('http://localhost:4001/events', event)
   axios.post('http://localhost:4002/events', event)
-  axios.post('http://localhost:4003/events', event)
+  axios.post('http://localhost:4003/events', event) */
   res.send({ status: 'OK' })
 })
 
